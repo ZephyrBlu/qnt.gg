cfa64ab697390fda6fb2d76965a0b752369ec729
ZephyrBlu <lukejholroyd@gmail.com>
2021-03-28 02:21:20 +1300

    Rename latest post + write more

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,5 @@
 export { postDetails as analyzingSc2 } from './analyzing-sc2.mdx';
-export { postDetails as financialQuantAnalysis } from './WIP-quant-finance-inspiration.mdx';
+export { postDetails as quantitativeFinanceAndGaming } from './WIP-quantitative-finance-and-gaming.mdx';
 export { postDetails as gamesAsTimeSeries } from './WIP-games-as-time-series.mdx';
 
 // next is dumb. have to do this to be able to build
