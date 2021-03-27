3759c6f89f6847d8a0d230f026c40517a45c695b
ZephyrBlu <lukejholroyd@gmail.com>
2021-03-27 16:10:01 +1300

    Dynamically generate exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,11 +1,39 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  exportPathMap: async () => ({
-    '/': { page: '/' },
-    '/analyzing-sc2': { page: '/blog/analyzing-sc2' },
-  }),
+  exportPathMap: async () => {
+      const postsDir = path.join(process.cwd(), 'pages/blog');
+      const pageRoutes = await fs.readdir(postsDir).then((posts) => {
+        const routes = {};
+        posts.forEach((name) => {
+          // don't care about index file
+          if (
+              name === 'index.js'
+              || (
+                process.env.NODE_ENV === 'production'
+                && name.slice(0, 4) === 'WIP-'
+              )
+            ) {
+            return;
+          }
+          // discard file extension
+          const noExt = name.split('.')[0];
+          routes[`/${noExt}`] = {
+            page: `/blog/${noExt}`,
+          };
+        });
+        return routes;
+      });
+
+      return {
+        '/': { page: '/' },
+        ...pageRoutes,
+      };
+    },
 });
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,4 +1,5 @@
 export { postDetails as analyzingSc2 } from './analyzing-sc2.mdx';
+export { postDetails as gamesAsTimeSeries } from './games-as-time-series.mdx';
 
 // next is dumb. have to do this to be able to build
 export default function NextIsDumb() {
