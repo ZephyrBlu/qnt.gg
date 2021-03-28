bc020521b8f2dd671abbebd533cfeedbe0a2485f
ZephyrBlu <lukejholroyd@gmail.com>
2021-03-29 00:04:01 +1300

    New post idea

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,5 @@
 export { postDetails as analyzingSc2 } from './analyzing-sc2.mdx';
 export { postDetails as quantitativeFinanceAndGaming } from './quantitative-finance-and-gaming.mdx';
-export { postDetails as predictingTrendsSc2 } from './WIP-predicting-trends-sc2.mdx';
 
 // next is dumb. have to do this to be able to build
 export default function NextIsDumb() {
