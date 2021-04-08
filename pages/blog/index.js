2cf1091fcf0e6f51e0cffd4d7738d3e99e5351ae
ZephyrBlu <lukejholroyd@gmail.com>
2021-04-08 22:06:09 +1200

    Minor edits to entropy post

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,6 @@
 export { postDetails as analyzingSc2 } from './analyzing-sc2.mdx';
 export { postDetails as quantitativeFinanceAndGaming } from './quantitative-finance-and-gaming.mdx';
+export { postDetails as informationTheoryScoutingSc2 } from './information-theory-scouting-sc2.mdx';
 
 // next is dumb. have to do this to be able to build
 export default function NextIsDumb() {
